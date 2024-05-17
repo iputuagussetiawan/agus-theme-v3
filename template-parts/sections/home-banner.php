078064760d7da7d236129e1e936190d54890edc9
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-05-17 09:36:24 +0800

    add slider text

--- template-parts/sections/home-banner.php
+++ template-parts/sections/home-banner.php
@@ -3,5 +3,20 @@
         <div class="topHead__title-wrapper">
             <h1 class="topHead__title">We Develop digital products from scratch</h1>
         </div>
+
+
+        <div class="topHead__inner">
+            <div class="wrapperRollingText">
+                <div class="rollingText text">
+                    Ultra high-performance, professional-grade animation for the modern web
+                </div>
+            </div>
+    
+            <div class="wrapperRollingText02">
+                <div class="rollingText02 text">
+                    <span>• GreenSock •</span> Ultra high-performance, professional-grade animation for the modern web
+                </div>
+            </div>  
+        </div>
     </div>
 </header>
